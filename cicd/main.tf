4931594e2f393038f0830fd9a13fd68ae9f9ca29
This-is-ravi <ravivenkat508@gmail.com>
2024-06-17 12:03:57 +0530

    backend

--- cicd/main.tf
+++ cicd/main.tf
@@ -29,6 +29,38 @@ module "jenkins_agent" {
   }
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMXIsn+d2qwXoo3isokCpTtwy3HBBk2dsLz9yqFAMWTZ AMARNATH@LAPTOP-6GDUPELH"
+  public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
+
+
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-02b3e7e7724074fae"]
+  # convert StringList to list and get first element
+  subnet_id = "subnet-05573cba2107b2ee0"
+  ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+  tags = {
+    Name = "nexus"
+  }
+}
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -53,6 +85,16 @@ module "records" {
         module.jenkins_agent.private_ip
       ]
       
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      allow_overwrite = true
+      records = [
+        module.nexus.private_ip   # why private ip means,evrytime we apply and destroy infra bcz of that pub,pri ips are changing, to stop that iam using private ip every were 
+      ]
+      allow_overwrite = true
     }
     
   ]
